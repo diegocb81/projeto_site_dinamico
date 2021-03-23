dfde625669f764bede896cd7e19f9600c4a2eb81
diegocb81 <diegocb81@gmail.com>
2021-03-23 20:59:35 -0300

    Desenvolvendo o projeto

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Projeto Site Dinâmico</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do website">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+</head>
+
+<body>
+    <header>
+        <div class="logo">Logomarca</div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços/a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços/a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--mobile-->
+    </header>
+
+    <section class="banner-principal">
+        <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar!">
+        </form>
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Diego de Castro Barcellos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta. Duis vel aliquet orci. Donec maximus et diam et consectetur. Donec in neque auctor, finibus metus consectetur, ultrices ex. In facilisis volutpat fermentum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta. Duis vel aliquet orci. Donec maximus et diam et consectetur. Donec in neque auctor, finibus metus consectetur, ultrices ex. In facilisis volutpat fermentum.</p>
+        </div><!--W50-->
+        <div class="w50">
+            <img src="img/foto.png" alt="Foto Autor">
+        </div><!--W50-->
+    </section><!--descricao-autor-->
+
+
+
+</body>
+
+</html>
