83c1c70fc850fa33fb40ac8b0e06e821bddfcbc9
diegocb81 <diegocb81@gmail.com>
2021-03-24 21:37:18 -0300

    Iniciando estilização

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Descrição do website">
     <meta name="keywords" content="palavras-chave,do,meu,site">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -45,32 +49,33 @@
 
     <section class="descricao-autor">
         <div class="center">
-            <div class="w50">
+            <div class="w50 left">
                 <h2>Diego de Castro Barcellos</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta. Duis vel aliquet orci. Donec maximus et diam et consectetur. Donec in neque auctor, finibus metus consectetur, ultrices ex. In facilisis volutpat fermentum.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta. Duis vel aliquet orci. Donec maximus et diam et consectetur. Donec in neque auctor, finibus metus consectetur, ultrices ex. In facilisis volutpat fermentum.</p>
             </div><!--W50-->
-            <div class="w50">
+            <div class="w50 left">
                 <img src="img/foto.png" alt="Foto Autor">
             </div><!--W50-->
+            <div class="clear"></div>
         </div><!--center-->
     </section><!--descricao-autor-->
 
     <section class="especialidades">
         <div class="center">
             <h2 class="title">Especialidades</h2>
-            <div class="box-especialidade">
-                <h3>icone1</h3>
+            <div class="w33 left box-especialidade">
+                <h3><em class="fab fa-css3-alt"></em></h3>
                 <h3>CSS3</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
             </div><!--box-especialidade-->
-            <div class="box-especialidade">
-                <h3>icone2</h3>
+            <div class="w33 left box-especialidade">
+                <h3><em class="fab fa-html5"></em></h3>
                 <h3>HTML5</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
             </div><!--box-especialidade-->
-            <div class="box-especialidade">
-                <h3>icone3</h3>
+            <div class="w33 left box-especialidade">
+                <h3><em class="fab fa-js-square"></em></h3>
                 <h3>JavaScript</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
             </div><!--box-especialidade-->
@@ -79,7 +84,7 @@
 
     <section class="extras">
         <div class="center">
-            <div class="w50">
+            <div class="w50 left">
                 <h2 class="title">Depoimentos</h2>
                 <div class="depoimento-single">
                     <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
@@ -94,7 +99,7 @@
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!--depoimento-single-->
             </div><!--w50-->
-            <div class="w50">
+            <div class="w50 left">
                 <h2 class="title">Serviços</h2>
                 <div class="servicos">
                     <ul>
@@ -104,6 +109,7 @@
                     </ul>
                 </div><!--servicos-->
             </div><!--w50-->
+            <div class="center"></div>
         </div><!--center-->
     </section><!--extras-->
 
@@ -112,9 +118,7 @@
             <p>Todos os direitos reservados.</p>
         </div><!--center-->
     </footer>
-
-
-
+    
 </body>
 
 </html>
