84a6208429b3d18bfbc77604188cd39db7b3ff8c
diegocb81 <diegocb81@gmail.com>
2021-04-01 14:16:58 -0300

    Criando pasta page e arquivo home.php

---
+++ pages/home.php
@@ -0,0 +1,79 @@
+<section class="banner-principal">
+        <div class="overlay"></div><!--overlay-->
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required>
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div><!--center-->
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Diego de Castro Barcellos</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta. Duis vel aliquet orci. Donec maximus et diam et consectetur. Donec in neque auctor, finibus metus consectetur, ultrices ex. In facilisis volutpat fermentum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta. Duis vel aliquet orci. Donec maximus et diam et consectetur. Donec in neque auctor, finibus metus consectetur, ultrices ex. In facilisis volutpat fermentum.</p>
+            </div><!--W50-->
+            <div class="w50 left">
+                <img class="right" src="<?php echo INCLUDE_PATH; ?>img/foto.png" alt="Foto Autor">
+            </div><!--W50-->
+            <div class="clear"></div>
+
+        </div><!--center-->
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidade">
+                <h3><em class="fab fa-css3-alt"></em></h3>
+                <h4>CSS3</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
+            </div><!--box-especialidade-->
+            <div class="w33 left box-especialidade">
+                <h3><em class="fab fa-html5"></em></h3>
+                <h4>HTML5</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
+            </div><!--box-especialidade-->
+            <div class="w33 left box-especialidade">
+                <h3><em class="fab fa-js-square"></em></h3>
+                <h4>JavaScript</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</p>
+            </div><!--box-especialidade-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </section><!--especialidades-->
+
+    <section class="extras">
+
+        <div class="center">
+            <div class="w50 left depoimentos-container">
+                <h2 class="title">Depoimentos dos nossos clientes</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta."</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta."</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta."</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+            </div><!--w50-->
+            <div class="w50 left servicos-container">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod nisi vel rhoncus ultrices. Suspendisse condimentum in elit in porta.</li>
+                    </ul>
+                </div><!--servicos-->
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </section><!--extras-->
