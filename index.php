401a90bc0eaff87e85825fa34f7c46ec94dc2515
diegocb81 <diegocb81@gmail.com>
2021-05-01 09:18:03 -0300

    Inserindo mapa na página contato

--- index.php
+++ index.php
@@ -66,7 +66,12 @@
 
     <script src="<?php echo INCLUDE_PATH; ?>js/jquery.js"></script>
     <script src="<?php echo INCLUDE_PATH; ?>js/scripts.js"></script>
-    
+    <?php
+        if($url == 'contato'){
+    ?>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDHPNQxozOzQSZ-djvWGOBUsHkBUoT_qH4"></script>
+    <script src="<?php echo INCLUDE_PATH; ?>js/map.js"></script>
+    <?php } ?>
 </body>
 
 </html>
