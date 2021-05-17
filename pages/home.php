317d880d13fbb96e47e6958589863a1db254ff1d
Diego Barcellos <diegocb81@gmail.com>
2021-05-17 18:10:24 -0300

    Adicionando as funções para os slides em slider.js

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,9 @@
-<section class="banner-principal">
-        <div class="overlay"></div><!--overlay-->
+<section class="banner-container">
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>img/bg-form.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>img/bg-form2.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>img/bg-form3.jpg');" class="banner-single"></div><!--banner-single-->
+    
+    <div class="overlay"></div><!--overlay-->
         <div class="center">
             <form>
                 <h2>Qual o seu melhor e-mail?</h2>
@@ -7,7 +11,7 @@
                 <input type="submit" name="acao" value="Cadastrar!">
             </form>
         </div><!--center-->
-    </section><!--banner-principal-->
+</section><!--banner-container-->
 
     <section class="descricao-autor">
         <div class="center">
