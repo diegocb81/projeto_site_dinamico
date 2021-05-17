d42a9e6087c2dfe6fc7c292d4744400085876025
Diego Barcellos <diegocb81@gmail.com>
2021-05-17 17:26:11 -0300

    Criando o arquivo slide.js

--- index.php
+++ index.php
@@ -82,6 +82,11 @@
 
     <script src="<?php echo INCLUDE_PATH; ?>js/jquery.js"></script>
     <script src="<?php echo INCLUDE_PATH; ?>js/scripts.js"></script>
+    <?php 
+        if($url == 'home' || $url == ''){
+    ?>
+    <script src="<?php echo INCLUDE_PATH; ?>js/slider.js"></script>
+    <?php } ?>
     <?php
         if($url == 'contato'){
     ?>
