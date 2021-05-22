d85f933a7f4214721bf0975c33cf7a4a9101ca7f
Diego Barcellos <diegocb81@gmail.com>
2021-05-22 08:35:56 -0300

    Atualizando Email.php

--- classes/Email.php
+++ classes/Email.php
@@ -5,6 +5,41 @@
         function __construct()
         {
             
+            $mail = new PHPMailer;
+
+            //Server settings
+            $mail->SMTPDebug = 0;                                       //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'vps.dast.com';                         //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'contato@dast.com';                     //SMTP username
+            $mail->Password   = 'secret';                               //SMTP password
+            $mail->SMTPSecure = 'ssl';                                  //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('contato@dast.com', 'Diego');
+            $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+            $mail->addAddress('ellen@example.com');               //Name is optional
+            $mail->addReplyTo('contato@dast.com', 'Diego');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+
+            //Attachments
+            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Assunto e-mail';
+            $mail->Body    = 'Corpo do <b>e-mail</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            if(!$mail->send()) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            } else {
+                echo "Message has been sent";
+            } 
         }
     }
 
